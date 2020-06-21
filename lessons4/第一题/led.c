23e513278f767e40e8dc7dc02e28ee52177258c3
unknown <yoy-hang>
2020-06-21 11:05:23 +0800

    第四次提交作业 by SuQihang

---
+++ lessons4/第一题/led.c
@@ -0,0 +1,82 @@
+#define IN1 2
+#define IN2 3
+#define IN3 4
+#define IN4 5
+
+#define S1 8
+#define S2 9
+#define S3 10
+#define S4 11
+
+#define LT 6
+#define BT 7
+void setup()
+{
+ 
+  Serial.begin(9600);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
+  pinMode(S4, OUTPUT);
+  
+  pinMode(LT,OUTPUT);
+  pinMode(BT,OUTPUT);
+  
+  digitalWrite(LT,HIGH);
+  digitalWrite(BT,HIGH);
+  
+  digitalWrite(S1,HIGH);
+  digitalWrite(S2,HIGH);
+  digitalWrite(S3,HIGH);
+  digitalWrite(S4,HIGH);
+  
+  digitalWrite(S1,LOW);
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,LOW);
+  digitalWrite(IN3,LOW);
+  digitalWrite(IN4,LOW);
+  digitalWrite(S1,HIGH);
+  
+    digitalWrite(S2,LOW);
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,LOW);
+  digitalWrite(IN3,LOW);
+  digitalWrite(IN4,LOW);
+  digitalWrite(S2,HIGH);
+  
+    digitalWrite(S3,LOW);
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,LOW);
+  digitalWrite(IN3,LOW);
+  digitalWrite(IN4,LOW);
+  digitalWrite(S3,HIGH);
+  
+    digitalWrite(S4,LOW);
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,LOW);
+  digitalWrite(IN3,LOW);
+  digitalWrite(IN4,LOW);
+  digitalWrite(S4,HIGH);
+  
+}
+
+byte income=0;
+void loop()
+{
+  if(Serial.available()>0)
+  {
+  	income=Serial.read();
+    
+    income=income-'0';
+    digitalWrite(S4,LOW);
+    digitalWrite(2,income&0x1);
+    digitalWrite(3,(income>>1)&0x1);
+    digitalWrite(4,(income>>2)&0x1);
+    digitalWrite(5,(income>>3)&0x1);
+    digitalWrite(S4,HIGH);
+  	delay(10);
+  }
+}
